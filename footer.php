300f97b6995120f976f9089f0a6e45fa24252857
Chethanaq <chethanaamin330@gmail.com>
2020-12-28 15:33:30 +0530

    frst push

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+  <div class="container">
+      <div class="footer-box">
+	  <?php 
+	        date_default_timezone_set('Asia/Kolkata');
+	           $date = date("Y-m-d");
+               $newDate = date("d-m-Y", strtotime($date));
+      ?>			   
+               
+        
+		</div>
+  </div>
+
+</body>
+</html>
